20d7878fe8800f0a0030257e399e68622dbdb09c
edi-rose <edirose1998@gmail.com>
2018-02-22 11:16:49 +1300

    added hourly wages to form and db

--- server/db/users.js
+++ server/db/users.js
@@ -1,11 +1,11 @@
 var hash = require('../auth/hash')
 
-function createUser (user_name, first_name, last_name, password, db) {
+function createUser (user_name, first_name, last_name, password, hourly_wage, db) {
   return new Promise ((resolve, reject) => {
     hash.generate(password, (err, hash) => {
       if (err) reject(err)
       db('users')
-        .insert({user_name, first_name, last_name, hash})
+        .insert({user_name, first_name, last_name, hourly_wage, hash})
         .then(user_id => resolve(user_id))
         .catch(err => reject(err))
     })
