f30d42220b00b5bffd796aa744213f432a8d86c4
Hayley Ha <hami201289@him-mail.ch>
2018-02-22 11:52:31 +1300

    still blocked

--- client/components/CreateMeeting.jsx
+++ client/components/CreateMeeting.jsx
@@ -9,30 +9,59 @@ class CreateMeeting extends React.Component {
       attendees: []
     }
     
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.person = { name: '', wage: 0}
     this.updateTitle = this.updateTitle.bind(this)
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    console.log(e.target.value)
+    this.updateName = this.updateName.bind(this)
+    this.updateWage = this.updateWage.bind(this)
+    this.addAttendee = this.addAttendee.bind(this)
   }
 
   updateTitle(e) {
     this.setState({title: e.target.value})
   }
 
+  updateName(e) {
+    console.log('name: ', e.target.value)
+    this.person.name = e.target.value
+  }
+
+  updateWage(e) {
+    console.log('wage: ', e.target.value)
+    this.person.wage = e.target.value
+  }
+
+  addAttendee(e) {
+    console.log('person: ', this.person)
+    let tmp = [...this.state.attendees].concat(this.person)
+    this.setState({attendees: tmp})
+    console.log('attendees: ', tmp)
+    console.log('this.state.attendees: ', this.state.attendees)
+    this.person.name = ''
+    this.person.wage = 0
+  }
+
+  displayAttendees() {
+    return this.state.attendees.map((person, i) => {
+      <li key={i}>{person.name}, {person.wage}</li>
+    })
+  }
 
   render() {
     return (
       <div className="container">
         <h2 className="title is-2">Create Meeting</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label for="title">Meeting title:</label>
-          <input id="title" type="text" onChange={this.updateTitle} />
-          <br />
-          <input type="submit" />
-        </form >
+        <label for="title">Meeting title:</label>
+        <input id="title" type="text" onChange={this.updateTitle} />
+        <br />
+        <ul>
+          { this.displayAttendees }
+        </ul>
+        <h3>Add Attendent</h3>
+        <label for="name">Attendee Name:</label>
+        <input id="name" type="text" onChange={this.updateName} />
+        <label for="wage">Attendee Wage:</label>
+        <input id="wage" type="text" onChange={this.updateWage} />  
+        <button onClick={this.addAttendee}>Add</button>
       </div >
     )
   }
