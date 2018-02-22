58f30f43fd850865a8f0a50e5572e7fe2dc97d9f
Maia <maiamiller25@gmail.com>
2018-02-22 14:55:13 +1300

    removed unnecessary text:

--- server/db/meetings.js
+++ server/db/meetings.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function saveMeeting(meeting, db) {
   createMeeting(meeting)
   .then((ids) => {
@@ -8,62 +7,15 @@ function saveMeeting(meeting, db) {
 }
 
 function createMeeting(meeting, db) {
-=======
-function saveMeeting(meeting) {
-  createMeeting(meeting)
-  addAttendeesToMeeting(meeting)
-}
-
-function createMeeting(meeting) {
->>>>>>> ba23537c950c7298151c4edaabd8d957e2f26ba4
   return db('meetings')
   .insert({meeting_name, cost, time, attendees: attendeesList.length})
 }
 
-<<<<<<< HEAD
 function addAttendeesToMeeting(meetingId, attendeesList, db) {
   let userAttendees = attendeesList.filter(person => person.id)
   return db('attendees')
   .insert({user_id: userAttendees.id, meeting_id: meetingId})
 }
-
-=======
-function addAttendeesToMeeting(meeting) {
-  let userAttendees = attendeesList.filter(person => person.id)
-  return db('attendees')
-  .insert({user_id: userAttendees.id, meeting_id:
-  })
-
-  })
-}
-
-
-return db('meetings')
-.insert({meeting_name, cost, time, attendees: attendeesList.length}).into('meetings')
-
-
-.insert(into users table with attendeesArr, ).into('users')
-.insert(then join table with meetingid and userid).into('attendees')
-
-
->>>>>>> ba23537c950c7298151c4edaabd8d957e2f26ba4
-meeting: {
-  meetingName: '',
-  time: '2017-02-22T10:00:00NZDT',
-  cost: 200.00,
-  attendeesList: [{
-    id:'',
-    firstName: '',
-    lastName: '',
-    hourlyRate: 50.00
-<<<<<<< HEAD
-  }],
-=======
-  }]
->>>>>>> ba23537c950c7298151c4edaabd8d957e2f26ba4
-}
-
-
 
 function getAttendees(meetingId, db) {
   return db('users')
