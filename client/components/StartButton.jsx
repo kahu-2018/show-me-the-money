af32ab99382a43dca39fe57bd5096c19d61643bb
edi-rose <edirose1998@gmail.com>
2018-02-22 20:00:38 +1300

    created attedees action, got start game button doing things

--- client/components/StartButton.jsx
+++ client/components/StartButton.jsx
@@ -1,20 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
 import Meeting from './Meeting'
 import {startMeeting, secondTick} from '../actions/currentMeeting'
+import {getAttendees} from '../actions/attendees'
 
-let arr = [{first_name: 'Edi', last_name: 'Rose', wage: '16'}, {first_name: 'Jay', last_name: 'and', wage: '16'}, {first_name: 'Silent', last_name: 'Bob', wage: '18'}]
+// let arr = [{first_name: 'Edi', last_name: 'Rose', wage: '16'}, {first_name: 'Jay', last_name: 'and', wage: '16'}, {first_name: 'Silent', last_name: 'Bob', wage: '18'}]
 
-class StartButton extends React.Component() {
-  constructor (props){
+class StartButton extends React.Component {
+  constructor(props) {
     super(props)
-    this.state ={
+    this.state = {
       showMeeting:false
     }
-    //bind here
+    this.handleClick = this.handleClick.bind(this)
   }
 
 handleClick() {
-  this.setState(showMeeting:true)
-  this.dispatch(startMeeting())
+  this.setState({showMeeting:true})
+  this.props.dispatch(startMeeting())
   this.setTimer()
 }
 
@@ -28,29 +31,36 @@ getWages(stuff){
 }
 
 setPerSecWages() {
- let wages = this.getWages(wage)
+ let wages = this.getWages(this.props.attendees)
  const combinedWages = wages.reduce((a, b) => a + b)
  const perSecondWages = (combinedWages / 60) / 60
  return perSecondWages
 }
 
 countTime() {
- dispatch(secondTick())
+ this.props.dispatch(secondTick())
+}
+
+componentDidMount() {
+  this.props.dispatch(getAttendees())
 }
 
 render () {
-
-  return
-
+  return (
   <div className="container">
     <h2 className="title is-2">Start Meeting</h2>
     <div className="startButton">
       <button class="button" onClick={this.handleClick}>Start Meeting</button>
     </div>
   </div>
+)
 }
 }
 
-mapStateToProps
+const mapStateToProps = (state) => {
+  return {
+    attendees:state.attendees
+  }
+}
 
 export default connect()(StartButton)
