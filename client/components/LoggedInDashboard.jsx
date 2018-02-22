ec6fbce8e53e6d37b3e571441bc8f3c63fd19703
Hayley Ha <hami201289@him-mail.ch>
2018-02-23 11:37:28 +1300

    loggedin dashboard still does not show

--- client/components/LoggedInDashboard.jsx
+++ client/components/LoggedInDashboard.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+// import React from 'react';
 
-import Header from './Header'
+// import Header from './Header'
 
 
-const loggedInDashboard = (props) => (
-    <div>
-        <Header />
-        <div className="columns">
-            <div className="column"><Link to={'/history'}>History</Link></div>
-            <div className="column"><Link to={'/meeting'}>Meeting</Link></div>
-            <div className="column"><Link to={'/profile'}>Profile</Link></div>
-        </div>
-    </div>
-);
+// const loggedInDashboard = (props) => {
+//     return (
+//     <div>
+//         <Header />
+//         <div className="columns">
+//             <div className="column"><Link to={'/history'}>History</Link></div>
+//             <div className="column"><Link to={'/meeting'}>Meeting</Link></div>
+//             <div className="column"><Link to={'/profile'}>Profile</Link></div>
+//         </div>
+//     </div>
+// )}
 
-export default loggedInDashboard;
+// export default loggedInDashboard;
+
+import React from 'react'
+import {connect} from 'react-redux'
+
+function LoggedInDashboard (props) {
+  return <div>
+           <Header />
+           <div className="columns">
+               <div className="column"><Link to={'/history'}>History</Link></div>
+               <div className="column"><Link to={'/meeting'}>Meeting</Link></div>
+               <div className="column"><Link to={'/profile'}>Profile</Link></div>
+           </div>
+       </div>
+}
+
+export default connect()(LoggedInDashboard)
