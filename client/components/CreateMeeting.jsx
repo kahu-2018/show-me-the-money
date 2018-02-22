347ca6ed2d32f2440c14843ac06940d801778a62
YWHo <YWHo@users.noreply.github.com>
2018-02-22 14:19:22 +1300

    enable add attendee

--- client/components/CreateMeeting.jsx
+++ client/components/CreateMeeting.jsx
@@ -6,8 +6,7 @@ class CreateMeeting extends React.Component {
     super(props)
     this.state = {
       title: '',
-      attendees: [],
-      count: 0
+      attendees: []
     }
 
     this.updateTitle = this.updateTitle.bind(this)
@@ -21,37 +20,22 @@ class CreateMeeting extends React.Component {
 
 
   addAttendee(e) {
-    console.log('formName: ', this.refs.formName.value)
     let person = {
       name: this.refs.formName.value,
       wage: this.refs.formWage.value
     }
     this.refs.formName.value = ''
     this.refs.formWage.value = ''
-    console.log('before this.state.attendees: ', this.state.attendees)
-    console.log('person: ', person)
     let newAttendees = [...this.state.attendees, person]
-    console.log('newAttendees: ', newAttendees)
     this.setState({
-      attendees: newAttendees,
-      count: this.state.count++
+      attendees: newAttendees
     })
-    console.log('this.state.attendees: ', this.state.attendees)
   }
 
   displayAttendees() {
-    console.log('result: ', results)
-    let samples = [
-      { name: 'happy', wage: '300' },
-      { name: 'hello', wage: '400' }
-    ]
-    let results = samples.map((person, i) => {
-      return <li key={i}>{person.name}, {person.wage}</li>
+    return this.state.attendees.map((person, i) => {
+        return <li key={i}>{person.name}, {person.wage}</li>
     })
-    return <ul>{results}</ul>
-    // return this.state.attendees.map((person, i) => {
-    //   return <li key={i}>{person.name}, {person.wage}</li>
-    // })
   }
 
   render() {
@@ -66,15 +50,17 @@ class CreateMeeting extends React.Component {
         <div>
           <h4 className="title is-4">Add Attendent</h4>
           <label>Name:</label>
-          <input ref="formName" type="text" /><br/>
+          <input ref="formName" type="text" /><br />
           <label>Wage:</label>
           <input ref="formWage" type="text" /><br />
           <button onClick={this.addAttendee}>Add</button>
         </div>
         <br />
         <div>
-        <h4 className="title is-4">Attendee list</h4>
-        {this.displayAttendees()}
+          <h4 className="title is-4">Attendee list</h4>
+          <ul>
+            {this.displayAttendees()}
+          </ul>
         </div>
       </div >
     )
