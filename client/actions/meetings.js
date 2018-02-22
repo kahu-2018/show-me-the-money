886bf8f6d28ec40ebc6a3232ac567e4b60660bb1
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-23 04:21:04 +1300

    getMeetings client side console loggind data from server

--- client/actions/meetings.js
+++ client/actions/meetings.js
@@ -1,5 +1,6 @@
+// import request from 'superagent'
 import request from '../utils/api'
-import { saveUserToken } from '../utils/auth'
+import {saveUserToken} from '../utils/auth'
 
 function receiveMeetings (meetings) {
   return {
@@ -9,24 +10,58 @@ function receiveMeetings (meetings) {
 }
 
 
-function addMeeting (meeting) {
+function requestMeeting (meeting) {
   return {
-    type: 'ADD_MEETING',
+    type: 'REQUEST_MEETING',
     meeting
   }
 }
 
+// export function getMeetings() {
+//   return dispatch => {
+//     request
+//     .get('/api/meetings')
+//     .end((err, res) => {
+//       if (err) {
+//         console.log(err.message)
+//       }
+//       console.log(res)
+//     })
+//   }
+// }
+
+
 export function getMeetings () {
-  return dispatch => {
-    return request('get', 'meetings' )
-    .then((response) => {
-      console.log('This is the getMeetings Request')
+  return function (dispatch) {
+    dispatch(requestMeeting())
+    request('get', 'meetings')
+    .then(res => {
+      console.log(res)
+      dispatch(receiveMeetings(res.body))
     })
-    .catch(err => {
-      console.log('ERROR: refer to getMeetings in meetings.js')
-    })
+    .catch(err => console.log(err.message))
   }
 }
+
+// .then((response) => {
+//   console.log(response)
+// })
+// .catch(err => {
+//   console.log(err.message)
+// })
+
+
+// export function getMeetings () {
+//   return dispatch => {
+//     return request('get', 'meetings' )
+//     .then((response) => {
+//       console.log('This is the getMeetings Request')
+//     })
+//     .catch(err => {
+//       console.log('ERROR: refer to getMeetings in meetings.js')
+//     })
+//   }
+// }
 
 export function postMeeting () {
   console.log('I am the postMeeting function')
--- client/components/History.jsx
+++ client/components/History.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {getMeetings} from '../actions/meetings'
 
-function History (props) {
-  return <div className="container">
+// function History (props) {
+//   return <div className="container">
+//     <h2 className="title is-2">Meeting history</h2>
+//   </div>
+// }
+
+class History extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.props.dispatch(getMeetings())
+  }
+  render() {
+    return <div className="container">
     <h2 className="title is-2">Meeting history</h2>
-  </div>
+    </div>
+  }
 }
+
+
 
 export default connect()(History)
