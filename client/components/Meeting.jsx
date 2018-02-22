4d7d6d970e623350131c915717b247a9a44ae560
kahu-2018 <fake@fake.com>
2018-02-23 10:37:11 +1300

    hiding form onclick

--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -6,16 +6,14 @@ import CreateMeeting from './CreateMeeting'
 function Meeting (props) {
 
   return <div className="container">
-    <CreateMeeting /> //need to move this to app or start button as meeting is child of startbutton
+    
     <h2 className="title is-2">Start Meeting</h2>
     <div className="startButton">
       <button class="button">Start Meeting</button>
       <button class="button">End Meeting</button>
       <button class="button">Reset Meeting</button>
     </div>
-
   </div>
-)
 }
 
 export default connect()(Meeting)
